3918de48b0c157b0a10846e0279396358a50705e
Tikare Muralidhara Pramod 1CS3 <00060437@rsint.net>
2019-11-19 10:29:27 +0000

    macro vs inline

---
+++ macrovsinline/mac_v_inline.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// calling printMax with the maximum of a and b
+#define CALL_WITH_MAX(a, b) printMax((a > b ? a : b))
+
+void printMax(int a)
+{
+  std::cout << "Max is :" << a << std::endl;
+}
+
+inline callWithMax(const& a, const& b)
+{
+  printMax((a > b ? a : b));
+}
+
+int main(int argc, char const* argv[])
+{
+  std::cout << "Hello\n";
+  int a;
+  a = 6;
+  // We would expect to call with a = 7 and b = 6
+  std::cout << "With Macro => ";
+  CALL_WITH_MAX(++a, 6); // Prints 8, because incremented twice
+  a = 6;
+  std::cout << "With inline => ";
+  callWithMax(++a, 6);
+  return 0;
+}
