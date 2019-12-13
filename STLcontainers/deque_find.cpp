4fab71326e03cdc7a2209d767ef52cf4abf2831e
Pramod Tikare <pramodkirchki@gmail.com>
2019-12-13 09:53:03 +0100

    myFind

--- STLcontainers/deque_find.cpp
+++ STLcontainers/deque_find.cpp
@@ -6,7 +6,15 @@
 
 const int SIZE = 10;
 
-// std::deque<int>::iterator myFind(std::deque<int> &);
+std::deque<int>::iterator myFind(std::deque<int> &myDeque, const int& value)
+{
+  for( auto it = myDeque.begin(); it != myDeque.end(); ++it)
+  {
+    if((*it) == value)
+      return it;
+  }
+  return myDeque.end();
+}
 
 int main(int argc, char** argv)
 {
@@ -15,22 +23,26 @@ int main(int argc, char** argv)
   std::mt19937 engine( static_cast<unsigned long>(time(0)) );
   std::uniform_int_distribution<int> dist(0, 100);
   std::deque<int> randNumbers;
-  
+  std::deque<int> myNumbers = {1,2,3,4,5,6,7,8,9,10};
   for (int i = 0; i < SIZE; ++i)
   {
     randNumbers.push_back(dist(engine));
   }
   
-  for( auto i : randNumbers ){
+  for( auto i : myNumbers ){
     std::cout << i << " ";
   }
   std::cout << "\n";
   
   int value = dist(engine);
-  std::deque<int>::iterator it = std::find( randNumbers.begin(), randNumbers.end(), value);
-  if( it!= randNumbers.end() ) std::cout << value << "found at " << it - randNumbers.begin();
+  value = 10;
+  std::deque<int>::iterator it = std::find( myNumbers.begin(), myNumbers.end(), value);
+  if( it!= myNumbers.end() ) std::cout << value << " found at " << it - myNumbers.begin() << "\n";
   else std::cout << value << " not found\n";
   
+  it = myFind(myNumbers, value);
+  if( it!= myNumbers.end() ) std::cout << value << " found at " << it - myNumbers.begin() << "\n";
+  else std::cout << value << " not found\n";
   
   return 0;
 }
