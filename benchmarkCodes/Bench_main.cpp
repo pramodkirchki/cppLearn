da17f298da3fb07efa5ee3e95f03d868f1d08e2b
Pramod <pramodkirchki@gmail.com>
2019-12-26 16:11:37 +0100

    Vec val vs Vec Ptr benchmark code

--- benchmarkCodes/Bench_main.cpp
+++ benchmarkCodes/Bench_main.cpp
@@ -1,6 +1,7 @@
 #include "benchmark/benchmark.h"
 
 #include "DequeLookup.h"
+#include "vecVal_vs_vecPtr.h"
 #include "virtualFunc_Bench.h"
 
 
--- benchmarkCodes/vecVal_vs_vecPoint.h
+++
@@ -1,9 +0,0 @@
-// A big object - Paticle??
-
-// vector of such objects
-
-// vector of unique pointers to the objects.
-
-// Reason?
-//  1. copying?
-//  2. Apply algorithms on vec of pointers => can also be applied to FTL containers?
