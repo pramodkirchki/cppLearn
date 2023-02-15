10c66ff797fc6b9f0756bd38330517015243fbf5
pramodkirchki <pramodkirchki@gmail.com>
2023-02-15 10:40:30 +0100

    Update Godbolt_StrictAliasingDemo.cpp

--- Godbolt_StrictAliasingDemo.cpp
+++ Godbolt_StrictAliasingDemo.cpp
@@ -1,21 +1,24 @@
 #include "stdint.h"
 #include<iostream>
 #include <cstring>
-#include<array>
-#include <set>
-typedef uint32_t myType;
+
+typedef uint64_t myType;
  uint32_t arr[2] = { 0x0000aa55, 0x55aa0000};
 // uint16_t arr[4] = { 0xaa55, 0x0000, 0x0000, 0x55aa};
-// uint8_t arr[8] = { 0x55, 0xaa, 0x00, 0x00, 0x00, 0x00, 0xaa, 0x55};
+// char arr[8] = { 0x55, 0xaa, 0x00, 0x00, 0x00, 0x00, 0xaa, 0x55};
 
+// Violates strict aliasing
+// pun to char and char to something else also violates strict aliasing rule
 void func1()
 {
-    myType *arr2 = (myType*)&arr;
+    auto *arr2 = reinterpret_cast<myType*>(reinterpret_cast<char*>(&arr));
 
     myType res = arr2[0] | arr2[1];
     std::cout << "func1 : Result : " << std::hex << static_cast<uint32_t>(res);
 }
 
+// Type pun using std::memcpy
+// If you really want to access one object using a pointer type of another object
 void func2()
 {
     myType arr2[8];
@@ -27,25 +30,23 @@ void func2()
 
 int main()
 {
-    std::array<std::array<int,3>,4> arr = {0};
     func1();
     func2();
-    std::set<uint8_t> testvector{2,4,2,1};
-    testvector.insert(5);
-    testvector.insert(0);
 
-    std::cout << "Printing array ";
-    for(auto a : arr)
-    {
-        for(auto b : a)
-        std::cout << b << " " ;
-    }
-    for(auto a: testvector)
-    {
-        std::cout << "\n" << static_cast<uint32_t>(a) << ", ";
-    }
-    std::cout << "\nSize : " << testvector.size();
-    int i = 0x10000 | 2;
-    std::cout << "\ni : " << i;
     return 0;
 }
+
+/*
+
+References:
+- https://accu.org/journals/overload/28/160/anonymous/
+- https://www.youtube.com/watch?v=_qzMpk-22cc&ab_channel=CppCon
+
+Notes:
+- Type casting v/s Type Punning. Type punning = When pointers are casted and alias the other. If 2 different pointers point to the same thing, they are aliasing.
+- Type punning can lead to UB if not done correctly - Compilers could optimize away store. -Wstrict-aliasing catches the violation
+- Type punning to the "effective" type allowed.
+- Type punning to char and back allowed.
+- Type punning to other types - Type pun correctly - using memcpy allowed - Optimizer replaces memcpy with mov.
+- Type punning to void and then from void to other type and back to void - not sure ?
+*/
